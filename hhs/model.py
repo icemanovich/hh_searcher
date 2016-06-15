ef3695be048db76687c42bfafe40cfcb7ec16fac
icemanovich <icemanovich@gmail.com>
2016-06-15 18:48:20 +0300

    Update __repr__ and __str__ Vacancy model methods

--- hhs/model.py
+++ hhs/model.py
@@ -11,6 +11,69 @@ class Vacancy(object):
         self.created_at = created_at
         self.published_at = published_at
 
+    def __repr__(self):
+        """ Detailed model view
+
+        :return: object
+        """
+        return 'Vacancy(%r, %r, %r, %r, %r, %r, %r, %r)' % (
+            self.name,
+            self.salary,
+            self.url,
+            self.archived,
+            self.area,
+            self.employer,
+            self.created_at,
+            self.published_at
+        )
+
+    def __str__(self):
+        """ Short description of filled model
+
+        :return: object
+        """
+        return '%s [ Salary: %s]: - %s (%s), %s' % (
+            self.name,
+            self.salary,
+            self.employer['name'],
+            self.employer['alternate_url'],
+            self.url
+        )
+
+    @classmethod
+    def from_dict(cls, data: dict) -> object:
+        """ Create model instance from dictionary
+
+        :param data:
+        :return: object
+        """
+        return cls(
+            url=data['alternate_url'],
+            archived=data['archived'],
+            area=data['area'],
+            employer=data['employer'],
+            name=data['name'],
+            salary=data['salary'],
+            created_at=data['created_at'],
+            published_at=data['published_at']
+        )
+
+    def to_dict(self) -> dict:
+        """ Convert filled model into dictionary
+
+        :return:
+        """
+        return dict(
+            url=self.url,
+            archived=self.archived,
+            area=self.area,
+            employer=self.employer,
+            name=self.name,
+            salary=self.salary,
+            created_at=self.created_at,
+            published_at=self.published_at
+        )
+
 
 class SearchQuery(object):
     def __init__(self, text, search_field, experience,
