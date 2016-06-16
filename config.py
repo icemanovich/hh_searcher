c52522e17c732c93f964a21fb4616aa9be8062f1
icemanovich <icemanovich@gmail.com>
2016-06-16 11:48:19 +0300

    Add dotEnv module

---
+++ config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from dotenv import load_dotenv
+import os
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+
+TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
+CHANNEL_NAME = os.environ.get('CHANNEL_NAME', '@HHSearcherBot')
